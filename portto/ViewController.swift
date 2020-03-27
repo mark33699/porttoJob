20762af2c3be29e61df9952214aa6ae15de1b752
mark <mark.appledeveloper@gmail.com>
2020-03-28 00:04:42 +0800

    create new project with icon & launchImage

---
+++ portto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  portto
+//
+//  Created by 謝飛飛 on 2020/3/27.
+//  Copyright © 2020 MarkFly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
